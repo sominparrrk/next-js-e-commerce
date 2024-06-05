30a5e74672a3492fd37e36238adbbb0a54cf42ed
sominparrrk <sompark98@gmail.com>
2024-06-05 16:45:29 +0100

    style: lint

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import styles from './Navbar.module.css';
 import { useState, useRef, useEffect, MutableRefObject } from 'react';
 import { CloseIcon } from '../icons/Close';
